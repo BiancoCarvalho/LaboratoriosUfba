6449102a524964f4073ba9e99fefa798848a88fa
BiancoCarvalho <biancocarvalho@hotmail.com>
2026-09-19 15:03:58 -0300

    Update lab-unblock.sh

--- lab-unblock.sh
+++ lab-unblock.sh
@@ -10,16 +10,13 @@ LOG="/var/log/lab.log"
 
 echo "[$(date '+%F %T')] host=$(hostname) UNBLOCK" >> "$LOG"
 
-# 1) fecha Firefox e mata tudo do 'prova'
 pkill -KILL -f firefox 2>/dev/null || true
 pkill -KILL -u "$USUARIO" 2>/dev/null || true
 
-# 2) remove o usuário 'prova'
 if id "$USUARIO" &>/dev/null; then
     userdel -r "$USUARIO" 2>/dev/null || true
 fi
 
-# 3) remove a regra de bloqueio do sudoers
 rm -f /etc/sudoers.d/99-prova-bloqueado
 
 echo "[$(date '+%F %T')] UNBLOCK concluído" >> "$LOG"
